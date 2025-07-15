c35653eb41703373a25f4de5c3841defbff5a385
supan <supan@ivotiontech.in>
2025-07-15 16:43:19 +0530

    js1

---
+++ javascript/first.js
@@ -0,0 +1,26 @@
+//data types
+//string
+//bollean
+//number
+//null
+//undefined
+//object alwaYS WITH key and valuee
+
+//type of:find for check datatype
+console.log(typeof("hello"))
+console.log(typeof(123))
+console.log(typeof(true))
+console.log(typeof(undefined))
+
+console.log(typeof(1+"string"))
+console.log(1+"new")
+console.log(5/"hyy")
+
+
+console.log(4-"new")
+
+
+// variable declaration 
+let abc = 4567
+console.log("your value is",abc)
+
